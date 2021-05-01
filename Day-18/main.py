28729c816710d9a3b6c7cf66d3792cf9eda3e740
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-01 14:50:44 +0530

    Add files via upload
    Move turtle randomly generating random colors from turtle library.

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,4 +1,5 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
 timmy = Turtle()
@@ -29,16 +30,44 @@ timmy = Turtle()
 #     timmy.pencolor(random.choice(color))
 #     draw_turtle_shapes(shape_side_num)
 
+# Move turtle randomly generating random colors from the list.
 
-color = ["spring green", "hot pink", "dark red", "medium violet red", "dark violet", "tomato", "dodger blue",
-         "gold2", "IndianRed2", "LightSeaGreen", "magenta", "MidnightBlue", "purple", "burlywood4"]
+# color = ["spring green", "hot pink", "dark red", "medium violet red", "dark violet", "tomato", "dodger blue",
+#          "gold2", "IndianRed2", "LightSeaGreen", "magenta", "MidnightBlue", "purple", "burlywood4"]
+# distance = 20
+# direction_list = [0, 90, 180, 270]
+# timmy.pensize(10)
+#
+# timmy.speed("fastest")
+# def move_timmy():
+#     timmy.color(random.choice(color))
+#     timmy.setheading(random.choice(direction_list))
+#     timmy.forward(50)
+#
+#
+# for _ in range(100):
+#     move_timmy()
+
+# Move turtle randomly generating random colors from turtle library.
+
 distance = 20
 direction_list = [0, 90, 180, 270]
 timmy.pensize(10)
-
 timmy.speed("fastest")
+
+turtle.colormode(255)
+
+
+def create_color():
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 def move_timmy():
-    timmy.color(random.choice(color))
+    timmy.color(create_color())
     timmy.setheading(random.choice(direction_list))
     timmy.forward(50)
 
