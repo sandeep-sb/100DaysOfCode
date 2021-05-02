1b8f34c4401b4b522ef24095aaa2c27779a2782b
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-02 16:29:43 +0530

    Etch-A-Sketch app

--- Day-19/main.py
+++ Day-19/main.py
@@ -4,12 +4,39 @@ mike = Turtle()
 screen = Screen()
 
 
+# turtle moves forward
 def moves_forward():
     mike.forward(10)
 
 
-screen.listen()
-screen.onkey(key="space", fun=moves_forward)
+# turtle moves backward
+def moves_backwards():
+    mike.backward(10)
 
+
+# screen is cleared and turtle gets back to its origin point
+def clear_screen():
+    mike.clear()
+    mike.reset()
+
+
+# turtle turns counter clockwise
+def turn_counter_clockwise():
+    new_heading = mike.heading() + 10
+    mike.setheading(new_heading)
+
+
+# turtle turns clockwise
+def turn_clockwise():
+    new_heading = mike.heading() - 10
+    mike.setheading(new_heading)
+
+
+screen.listen()
+screen.onkeypress(key="w", fun=moves_forward)
+screen.onkeypress(key="s", fun=moves_backwards)
+screen.onkeypress(key="c", fun=clear_screen)
+screen.onkeypress(key="a", fun=turn_counter_clockwise)
+screen.onkeypress(key="d", fun=turn_clockwise)
 
 screen.exitonclick()
