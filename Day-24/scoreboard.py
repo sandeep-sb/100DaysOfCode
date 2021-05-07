348f3f47ebc78a02fce62b58732fe01ab37fc9de
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-07 23:54:07 +0530

    updated scoreboard.py from day-21

---
+++ Day-24/scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        with open("highscore_file.txt") as file:
+            self.high_score  = int(file.read())
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(x=0, y=265)
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Score = {self.score}   High Score = {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("highscore_file.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(arg="Game Over", align=ALIGNMENT, font=FONT)
+
+    def refresh(self):
+        self.score += 1
+        self.update_scoreboard()
