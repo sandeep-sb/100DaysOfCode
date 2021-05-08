915129b9bf6632f6a17e8df8490c8ac346368e83
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-08 21:07:55 +0530

    Add files via upload

---
+++ Day-25/main.py
@@ -0,0 +1,32 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+text = turtle.Turtle()
+text.hideturtle()
+text.penup()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state "
+                                                                                             "name?").title()
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        state_data = data[data.state == answer_state]
+        x_cor = float(state_data.x)
+        y_cor = float(state_data.y)
+
+        text.goto(x=x_cor, y=y_cor)
+        text.write(f"{answer_state}")
+
+
+turtle.mainloop()
+screen.exitonclick()
