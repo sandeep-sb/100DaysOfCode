d5fc6ef5cc5ed620d7bea6645a506e4c763d4a51
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-13 19:34:17 +0530

    Add files via upload

--- Day-29 Paasword Manager/main.py
+++ Day-29 Paasword Manager/main.py
@@ -1,7 +1,31 @@
+import random
 from tkinter import *
+from tkinter import messagebox
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    pass_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    pass_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    pass_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = pass_numbers + pass_letters + pass_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    # password = ""
+    # for char in password_list:
+    #     password += char
+
+    password_entry.insert(0, string=password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -9,11 +33,18 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    with open("data.txt", "a") as data:
-        data.write(f"{website}  |  {email}  |  {password}\n")
-    website_entry.delete(0, "end")
-    password_entry.delete(0, "end")
-    website_entry.focus()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_okay = messagebox.askokcancel(title=website, message=f"These are the details entered:\n Email: {email}\n "
+                                                                f"Password: {password}\n Is it ok to save?")
+        if is_okay:
+            with open("data.txt", "a") as data:
+                data.write(f"{website}  |  {email}  |  {password}\n")
+            website_entry.delete(0, "end")
+            password_entry.delete(0, "end")
+            website_entry.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -45,7 +76,7 @@ password_entry = Entry(width=21, show="*")
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Button
-generate_pass_button = Button(text="Generate Password")
+generate_pass_button = Button(text="Generate Password", command=generate_password)
 generate_pass_button.grid(row=3, column=2, sticky="EW")
 add_button = Button(width=36, text="Add", command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
