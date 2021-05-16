4128113a908189bd3a3c1b960328d261846cf040
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-05-16 23:55:09 +0530

    Add files via upload

---
+++ List Revision/panic2.py
@@ -0,0 +1,14 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+# Manipulation using list slicing
+plist = plist[1:8]
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
