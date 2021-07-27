2493daa4ed743cf202162d74589a8ebe876c39d2
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-07-27 23:05:44 +0530

    Add files via upload

--- Day-54 Hello_Flask/main.py
+++ Day-54 Hello_Flask/main.py
@@ -3,6 +3,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+
+    return wrapper
+
+
+def make_emphasis(f):
+    def wrapper():
+        return f"<em>{f()}</em>"
+
+    return wrapper
+
+
+def make_underlined(fun):
+    def wrapper():
+        return f"<u>{fun()}</u>"
+
+    return wrapper
+
+
 @app.route("/")
 def hello_world():
     return '<h1 style= "text-align: center">Hello World!</h1>' \
@@ -10,17 +31,12 @@ def hello_world():
            '<img src="https://media.giphy.com/media/hvS1eKlR75hMr0l7VJ/giphy.gif" width=200>'
 
 
-def make_bold(fun):
-    fun = "<b>"
-
-
-make_bold()
-
-
 @app.route("/bye")
 @make_bold
+@make_emphasis
+@make_underlined
 def bye():
-    return "bye!!"
+    return "Bye!!"
 
 
 @app.route('/username/<path:name>')
