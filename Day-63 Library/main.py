83536f19cc4371efe892a946baf5115c28311fd0
sandeep-sb <83261468+sandeep-sb@users.noreply.github.com>
2021-08-12 23:42:36 +0530

    Add files via upload

--- Day-63 Library/main.py
+++ Day-63 Library/main.py
@@ -9,12 +9,13 @@ from flask_sqlalchemy import SQLAlchemy
 # #                " author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///new-books-collection.db'
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 app.config["SECRET_KEY"] = "My library project"
-
+db = SQLAlchemy(app)
 
 # Create Table
 class Book(db.Model):
@@ -31,6 +32,9 @@ db.create_all()
 # new_book = Book(id=1, title="Murder On the Orient Express", author="Agatha Christie", rating=9.6)
 # db.session.add(new_book)
 # db.session.commit()
+
+# Read all the data
+# all_books_data = Book.query.all()
 
 # Read data by using particular query
 # read = Book.query.filter_by(title="Hercule Poirot").first()
@@ -54,15 +58,47 @@ db.create_all()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    all_books = Book.query.all()
+    return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        pass
+        print(request.form)
+        try:
+            new_book = Book(title=request.form["title"],
+                            author=request.form["author"],
+                            rating=request.form['rating']
+                            )
+            db.session.add(new_book)
+            db.session.commit()
+        except IntegrityError:  # this is done so that titles and authors are not repeated.
+            return redirect(url_for("add"))
         return redirect(url_for('home'))
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit_rating():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        update_record = Book.query.get(book_id)
+        update_record.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_id = request.args.get("book_id")
+    book_selected = Book.query.get(book_id)
+    return render_template("edit.html", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("book_id")
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
